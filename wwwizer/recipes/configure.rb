34784ef6f9c5b5ce635bf9a1c8a677b6fba11ea2
huming <ming@strikingly.com>
2014-07-04 12:28:00 +0800

    file creation

--- wwwizer/recipes/configure.rb
+++ wwwizer/recipes/configure.rb
@@ -1,10 +1,9 @@
 
 file_name = node[:wwwwizer][:say_something]
 
-# puts node.inspect
-# file "/etc/nginx/#{file_name}" do
-#   owner "root"
-#   group "root"
-#   mode "0755"
-#   action :create
-# end
+file "/etc/nginx/#{file_name}" do
+  # owner "root"
+  # group "root"
+  # mode "0755"
+  action :create
+end
